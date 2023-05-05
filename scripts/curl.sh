d25f2ee3d01f43776e570a4d620feee9423b2a69
liuqiming.lqm <liuqiming.lqm@alibaba-inc.com>
2023-05-05 13:25:33 +0800

    fix curl download

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -4,15 +4,17 @@ export CURL_RETRY_OPTS="--retry 5 --retry-delay 1"
 export CURL_FAILED_OPTS="--fail --continue-at -"
 
 function curl_download() {
-    curl ${CURL_VERBOSE_OPTS} ${CURL_DOWNLOAD_OPTS} ${CURL_RETRY_OPTS} ${CURL_FAILED_OPTS} $@
+  curl ${CURL_VERBOSE_OPTS} ${CURL_DOWNLOAD_OPTS} ${CURL_RETRY_OPTS} ${CURL_FAILED_OPTS} $@
 }
 
 function curl_download_to_file() {
-    local file=${1}
-    curl_download -o "${file}"
+  local url=${1}
+  local file=${2}
+  curl_download -o "${file}" ${url}
 }
 
 function curl_download_to_dir() {
-    local dir=${1:-${PWD}}
-    curl_download -O --output-dir "${dir}"
+  local url=${1}
+  local dir=${2:-${PWD}}
+  curl_download -O --output-dir "${dir}" ${url}
 }
