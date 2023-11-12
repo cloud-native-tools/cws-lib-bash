c8d1bb2abaf05ec911f4fa8450503ddaf0cc0889
Qiming, Liu <liuqiming.lqm@alibaba-inc.com>
2023-11-12 11:50:38 +0800

    rename timestamp2date

--- profile.d/10_datetime.sh
+++ profile.d/10_datetime.sh
@@ -1,8 +1,7 @@
-function timestamp2date() {
+function date_from_timestamp() {
   date '+%Y-%m-%d %T' --date @${1}
 }
 
 function date_adjust_seconds() {
   date '+%Y-%m-%d %T' --date="$1 CST $2 seconds"
 }
-
