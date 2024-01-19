37aa740f1bd92fa412d6c6d4047fee7c3079284e
Qiming, Liu <liuqiming.lqm@alibaba-inc.com>
2024-01-19 15:46:15 +0800

    update remote config for ssh

--- profile.d/08_remote.sh
+++ profile.d/08_remote.sh
@@ -1,72 +1,74 @@
+HOSTS_FILE_ROOT="${HOME}/.remote"
+SSH_CONFIG_FILE="${HOME}/.ssh/config"
+
 function remote_get_hosts() {
-    local hosts_file_root="${HOME}/.remote"
-    local hosts_file=${1:-remote_hosts}
-    if ! cat ${hosts_file_root}/${hosts_file}; then
-        log error "$? cannot find ${hosts_file_root}/${hosts_file}"
-    fi
+  local hosts_file=${1:-remote_hosts}
+  if ! cat ${HOSTS_FILE_ROOT}/${hosts_file}; then
+    log error "$? cannot find ${HOSTS_FILE_ROOT}/${hosts_file}"
+  fi
 }
 
 function remote_upload() {
-    local dest=$1
-    shift
-    local src=$@
-    if [ -z "${dest}" ] || [ -z "${src}" ]; then
-        log warn "Usage: remote_upload <dest> <src1> [src2] ..."
-        return ${RETURN_FAILURE}
-    fi
-    for host in $(remote_get_hosts); do
-        log info "Deploy [${src}] to ${host}:${dest}"
-        scp -r ${src} ${host}:${dest}
-    done
+  local dest=$1
+  shift
+  local src=$@
+  if [ -z "${dest}" ] || [ -z "${src}" ]; then
+    log warn "Usage: remote_upload <dest> <src1> [src2] ..."
+    return ${RETURN_FAILURE}
+  fi
+  for host in $(remote_get_hosts); do
+    log info "Deploy [${src}] to ${host}:${dest}"
+    scp -r ${src} ${host}:${dest}
+  done
 }
 
 function remote_cmd() {
-    for host in $(remote_get_hosts); do
-        log plain "Run on [${host}]: [$@]"
-        log plain "---"
-        ssh -t -q ${host} -- "bash -l -c '$@';echo \"Exit Code: $?\""
-        log plain "---"
-    done
+  for host in $(remote_get_hosts); do
+    log plain "Run on [${host}]: [$@]"
+    log plain "---"
+    ssh -t -q ${host} -F ${SSH_CONFIG_FILE} -- "bash -l -c '$@';echo \"Exit Code: $?\""
+    log plain "---"
+  done
 }
 
 function remote_cmd_expect() {
-    local password=$1
-    shift
-    for host in $(remote_get_hosts); do
-        log plain "Run on [${host}]: [$@]"
-        log plain "---"
-        expect <<-EOF
-    spawn ssh -t -q ${host} -- "bash -l -c '$@'"
+  local password=$1
+  shift
+  for host in $(remote_get_hosts); do
+    log plain "Run on [${host}]: [$@]"
+    log plain "---"
+    expect <<-EOF
+    spawn ssh -t -q ${host} -F ${SSH_CONFIG_FILE} -- "bash -l -c '$@'"
     expect '*password:'
     send '${password}\r'
 EOF
-        log plain "---"
-    done
+    log plain "---"
+  done
 
 }
 
 function remote_sync_hostname() {
-    for host in $(remote_get_hosts); do
-        log plain "Run on [${host}]: [$@]"
-        log plain "---"
-        ssh -t -q ${host} -- "echo ${host} > /etc/hostname; hostname ${host}; hostname"
-        log plain "---"
-    done
+  for host in $(remote_get_hosts); do
+    log plain "Run on [${host}]: [$@]"
+    log plain "---"
+    ssh -t -q ${host} -F ${SSH_CONFIG_FILE} -- "echo ${host} > /etc/hostname; hostname ${host}; hostname"
+    log plain "---"
+  done
 }
 
 function remote_download() {
-    local target=${1}
-    local root=${2}
-    if [ -n "${target}" ]; then
-        if [ -z "${root}" ]; then
-            root=.
-        fi
-        for host in $(remote_get_hosts); do
-            log info "Get ${host}:${target} ${root}"
-            mkdir -pv $(dirname ${root}/${host}/${target})
-            scp -r ${host}:${target} ${root}/${host}/${target}
-        done
-    else
-        log warn "Usage: remote_download <abs path> "
+  local target=${1}
+  local root=${2}
+  if [ -n "${target}" ]; then
+    if [ -z "${root}" ]; then
+      root=.
     fi
+    for host in $(remote_get_hosts); do
+      log info "Get ${host}:${target} ${root}"
+      mkdir -pv $(dirname ${root}/${host}/${target})
+      scp -r ${host}:${target} ${root}/${host}/${target}
+    done
+  else
+    log warn "Usage: remote_download <abs path> "
+  fi
 }
