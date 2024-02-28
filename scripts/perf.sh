499c8cde09f590a91a5187b95db47e056c39deb2
Qiming, Liu <liuqiming.lqm@alibaba-inc.com>
2024-02-28 15:28:55 +0800

    add perf script

--- scripts/perf.sh
+++ scripts/perf.sh
@@ -2,16 +2,19 @@ function perf_trace_net() {
   perf trace --no-syscalls --event 'net:*' $@ >/dev/null
 }
 
-function perf_syscall_record() {
-  local iteration=${1:-10}
-  local duration=${2:-5}
-  for iter in $(seq ${iteration}); do
-    log info "Iter: ${iter}"
-    perf record -e 'raw_syscalls:*' -a -o node-trace-$(date '+%s').data -- sleep ${duration}
-  done
+function perf_record_syscall() {
+  local output=${3:-perf-record-$(date '+%s').data}
+  local duration=${2:-60}
+  perf record -a -e 'raw_syscalls:*' -o ${output} -- sleep ${duration}
 }
 
-function perf_syscall_report() {
+function perf_trace_syscall() {
+  local output=${1:-perf-trace-$(date '+%s').log}
+  local duration=${2:-120}
+  perf trace -a -e 'syscalls:sys_enter_*' -o ${output} -- sleep ${duration}
+}
+
+function perf_trace_report() {
   local input=${1}
   perf trace -S -i ${input}
 }
@@ -25,9 +28,23 @@ function perf_cgroup_path_of_pid() {
 
 function perf_record_by_cgroup() {
   local cgroup_path=${1}
+  if [ -z "${cgroup_path}" ]; then
+    echo "Usage: perf_record_by_cgroup <cgroup_path>"
+    return 1
+  fi
   shift
-  local perf_options=${@}
-  perf record -e cpu-clock -g ${perf_options} -G ${cgroup_path}
+  perf record -e cpu-clock -g -G ${cgroup_path} $@
+}
+
+function perf_record_pod() {
+  local pod_uuid=${1}
+  local pod_sid=${2}
+  if [ -z "${pod_uuid}" ] || [ -z "${pod_sid}" ]; then
+    echo "Usage: perf_record_pod <pod_uuid> <pod_sid>"
+    return 1
+  fi
+  shift 2
+  perf_record_by_cgroup "kubepods/burstable/pod${pod_uuid}/${pod_sid}" -- ${@}
 }
 
 function perf_record_by_cgroup_of_pid() {
@@ -41,8 +58,21 @@ function perf_report() {
   local perf_data=${1}
   local pid=${2}
   if [ -z "${pid}" ]; then
-      perf report -i ${perf_data}
+    perf report -i ${perf_data}
   else
-      perf report -i ${perf_data} --kallsyms=/proc/kallsyms --symfs=/proc/${pid}/root
+    perf report -i ${perf_data} --kallsyms=/proc/kallsyms --symfs=/proc/${pid}/root
   fi
+}
+
+function perf_profiling_pid() {
+  local pid=${1}
+  local duration=${2:-600}
+  perf record -o perf-${pid}.data -p ${pid} -F 999 -g -- sleep ${duration}
+  perf script -i perf-${pid}.data --symfs=/proc/${pid}/root >perf-${pid}.txt
+}
+
+function perf_profiling() {
+  local duration=${1:-120}
+  perf record -o perf.data -a -F 999 -g -- sleep ${time}
+  perf script -i perf.data >perf.txt
 }
