2876fa9757f97f01429c881d1efe34652bc0859d
Qiming, Liu <liuqiming.lqm@alibaba-inc.com>
2024-03-13 01:06:16 +0800

    add a yum install with retry to make sure install success

--- scripts/yum.sh
+++ scripts/yum.sh
@@ -31,3 +31,30 @@ function yum_mk_rootfs() {
 function yum_list() {
   yum list --showduplicates $@
 }
+
+function yum_install_with_retry() {
+  local retries=${1}
+  local count=0
+
+  if [ $# -lt 2 ]; then
+    echo "Usage: yum_install_with_retry <retries> <package>"
+    return ${RETURN_FAILURE}
+  fi
+
+  shift
+
+  while [ $count -lt $retries ]; do
+    if yum install -y $@; then
+      return ${RETURN_SUCCESS}
+    else
+      echo "install failed at $count attempt, need retry: $@"
+      count=$(($count + 1))
+      sleep 1
+    fi
+  done
+
+  if [ $count -eq $retries ]; then
+    echo "Failed to install package after $retries attempts."
+    return ${RETURN_FAILURE}
+  fi
+}
