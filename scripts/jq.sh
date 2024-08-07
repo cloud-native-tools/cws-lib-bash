aaf97ffc6147357f46b0693da88ede9d5ceacc7d
Qiming, Liu <liuqiming.lqm@alibaba-inc.com>
2024-08-07 19:32:34 +0800

    add a jq_format function

--- scripts/jq.sh
+++ scripts/jq.sh
@@ -1,10 +1,17 @@
 function jq_array_add() {
   local field=${1}
   local new_array=${2}
-  cat - | jq ".${field} = . + ${new_array}"
+  local json_file=${3:--}
+  cat ${json_file} | jq ".${field} = . + ${new_array}"
 }
 
 function jq_dict_add() {
   local new_dict=${1}
-  cat - | jq ". = . + ${new_dict}"
+  local json_file=${2:--}
+  cat ${json_file} | jq ". = . + ${new_dict}"
+}
+
+function jq_format() {
+  local json_file=${1:--}
+  cat ${json_file} | jq .
 }
