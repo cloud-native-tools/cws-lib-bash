d01bbda8429a85e582198b8af7bb7059885599f2
Qiming, Liu <liuqiming.lqm@alibaba-inc.com>
2024-09-06 11:57:03 +0800

    add new function

--- profile.d/05_file.sh
+++ profile.d/05_file.sh
@@ -236,3 +236,22 @@ function fix_files_name() {
     popd
   done
 }
+
+function fix_permissions() {
+  for d in "$@"; do
+    find "${d}" \
+      ! \( \
+      -group "${NB_GID}" \
+      -a -perm -g+rwX \
+      \) \
+      -exec chgrp "${NB_GID}" -- {} \+ \
+      -exec chmod g+rwX -- {} \+
+    # setuid, setgid *on directories only*
+    find "${d}" \
+      \( \
+      -type d \
+      -a ! -perm -6000 \
+      \) \
+      -exec chmod +6000 -- {} \+
+  done
+}
--- scripts/convert.sh
+++
@@ -1,6 +0,0 @@
-function mov2gif() {
-  ffmpeg -i "$1" -pix_fmt rgb24 -r 10 "$2"
-}
-function optimize_gif() {
-  gifsicle --optimize=3 --delay=3 "$1" -o "$2"
-}
