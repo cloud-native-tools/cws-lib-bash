820cb30a409be856126e865b0f82a84d9af09954
Qiming Liu (John) <john.liuqiming@linux.alibaba.com>
2025-05-19 17:12:44 +0800

    update copilot_generate_instructions

--- scripts/copilot.sh
+++ scripts/copilot.sh
@@ -1,6 +1,17 @@
 function copilot_generate_instructions() {
   mkdir -pv docs
-  touch docs/instructions.md
+  if [ ! -f docs/instructions.md ]; then
+    if [ -f config/instructions.md ]; then
+      mv -fv config/instructions.md docs/instructions.md
+    else
+      cat <<EOF >docs/instructions.md
+## 代码生成
+- 如果目录中包含README.md文件需要参考其内容
+- 如果需要完成的动作较多，就先把需要做的动作写入一个TODO.md文件中,然后再进行代码生成
+- 如果文件比较大，生成代码之后不需要进行错误修复
+EOF
+    fi
+  fi
 
   mkdir -p .clinerules
   pushd .clinerules
