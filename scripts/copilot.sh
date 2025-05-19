66e2b3ab5508dec7650169a0203955eed93698e8
Qiming Liu (John) <john.liuqiming@linux.alibaba.com>
2025-05-19 16:49:29 +0800

    rename instructions.md path

--- scripts/copilot.sh
+++ scripts/copilot.sh
@@ -1,24 +1,24 @@
 function copilot_generate_instructions() {
-  mkdir -p config
-  touch config/instructions.md
+  mkdir -pv docs
+  touch docs/instructions.md
 
   mkdir -p .clinerules
   pushd .clinerules
-  ln -sf ../config/instructions.md project_rules.md
+  ln -sfv ../docs/instructions.md project_rules.md
   popd
 
   mkdir -p .github
   pushd .github
-  ln -sf ../config/instructions.md copilot-instructions.md
+  ln -sfv ../docs/instructions.md copilot-instructions.md
   popd
 
   mkdir -p .lingma/rules
   pushd .lingma/rules
-  ln -sf ../../config/instructions.md project_rule.md
+  ln -sfv ../../docs/instructions.md project_rule.md
   popd
 
   mkdir -p .trae/rules
   pushd .trae/rules
-  ln -sf ../../config/instructions.md project_rules.md
+  ln -sfv ../../docs/instructions.md project_rules.md
   popd
 }
